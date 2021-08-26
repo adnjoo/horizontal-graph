917ca790729821cd3e0cd4d2ec3e400f4b884c0a
Andrew Njoo <adnjoo@gmail.com>
2021-08-26 12:54:00 -0700

    refactor

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const button = document.getElementById("button");
 
 //sort function
 let mySort = (myVar) => {
-  console.log(myVar)
+  console.log(myVar);
   //clear container
   container.innerHTML = "";
 
@@ -51,7 +51,12 @@ let mySort = (myVar) => {
   });
 
   //find max variable
-  let max = Math.max.apply(Math, data.map(function(data) { return data[myVar]; }))
+  let max = Math.max.apply(
+    Math,
+    data.map(function (data) {
+      return data[myVar];
+    })
+  );
   max = max * 1.25;
   //loop through data and append name and age
   for (i in data) {
@@ -66,7 +71,7 @@ let mySort = (myVar) => {
     let age = document.createElement("td");
     age.classList.add("td");
     let div = document.createElement("div");
-    div.style.width = data[i][myVar]/max*100 + '%';
+    div.style.width = (data[i][myVar] / max) * 100 + "%";
     div.classList.add("age");
     //check if first or last
     if (i == 0) {
@@ -81,75 +86,75 @@ let mySort = (myVar) => {
   }
 };
 
-mySort('age');
+mySort("age");
 
 //dropdown code
-function toggleClass(elem,className){
-    if (elem.className.indexOf(className) !== -1){
-      elem.className = elem.className.replace(className,'');
-    }
-    else{
-      elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
-    }
-  
-    return elem;
+function toggleClass(elem, className) {
+  if (elem.className.indexOf(className) !== -1) {
+    elem.className = elem.className.replace(className, "");
+  } else {
+    elem.className = elem.className.replace(/\s+/g, " ") + " " + className;
   }
-  
-  function toggleDisplay(elem){
-    const curDisplayStyle = elem.style.display;			
-  
-    if (curDisplayStyle === 'none' || curDisplayStyle === ''){
-      elem.style.display = 'block';
-    }
-    else{
-      elem.style.display = 'none';
-    }
-  
+
+  return elem;
+}
+
+function toggleDisplay(elem) {
+  const curDisplayStyle = elem.style.display;
+
+  if (curDisplayStyle === "none" || curDisplayStyle === "") {
+    elem.style.display = "block";
+  } else {
+    elem.style.display = "none";
   }
-  
-  function toggleMenuDisplay(e){
-    const dropdown = e.currentTarget.parentNode;
-    const menu = dropdown.querySelector('.menu');
-    const icon = dropdown.querySelector('.fa-angle-right');
-  
-    toggleClass(menu,'hide');
-    toggleClass(icon,'rotate-90');
+}
+
+function toggleMenuDisplay(e) {
+  const dropdown = e.currentTarget.parentNode;
+  const menu = dropdown.querySelector(".menu");
+  const icon = dropdown.querySelector(".fa-angle-right");
+
+  toggleClass(menu, "hide");
+  toggleClass(icon, "rotate-90");
+}
+
+function handleOptionSelected(e) {
+  toggleClass(e.target.parentNode, "hide");
+
+  const id = e.target.id;
+  const newValue = e.target.textContent + " ";
+  const titleElem = document.querySelector(".dropdown .title");
+  const icon = document.querySelector(".dropdown .title .fa");
+
+  titleElem.textContent = newValue;
+  titleElem.appendChild(icon);
+
+  //trigger custom event
+  document.querySelector(".dropdown .title").dispatchEvent(new Event("change"));
+  //setTimeout is used so transition is properly shown
+  setTimeout(() => toggleClass(icon, "rotate-90", 0));
+}
+
+function handleTitleChange(e) {
+  console.log(e.target.textContent);
+  if (e.target.textContent.includes("salary")) {
+    mySort("salary");
+  } else {
+    mySort("age");
   }
-  
-  function handleOptionSelected(e){
-    toggleClass(e.target.parentNode, 'hide');			
-  
-    const id = e.target.id;
-    const newValue = e.target.textContent + ' ';
-    const titleElem = document.querySelector('.dropdown .title');
-    const icon = document.querySelector('.dropdown .title .fa');
-  
-  
-    titleElem.textContent = newValue;
-    titleElem.appendChild(icon);
-  
-    //trigger custom event
-    document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
-      //setTimeout is used so transition is properly shown
-    setTimeout(() => toggleClass(icon,'rotate-90',0));
-  }
-  
-  function handleTitleChange(e){
-    console.log(e.target.textContent)
-    if (e.target.textContent.includes("salary")) {
-        mySort('salary');
-      } else {
-        mySort('age');
-      }
-  }
-  
-  //get elements
-  const dropdownTitle = document.querySelector('.dropdown .title');
-  const dropdownOptions = document.querySelectorAll('.dropdown .option');
-  
-  //bind listeners to these elements
-  dropdownTitle.addEventListener('click', toggleMenuDisplay);
-  
-  dropdownOptions.forEach(option => option.addEventListener('click',handleOptionSelected));
-  
-  document.querySelector('.dropdown .title').addEventListener('change',handleTitleChange);
+}
+
+//get elements
+const dropdownTitle = document.querySelector(".dropdown .title");
+const dropdownOptions = document.querySelectorAll(".dropdown .option");
+
+//bind listeners to these elements
+dropdownTitle.addEventListener("click", toggleMenuDisplay);
+
+dropdownOptions.forEach((option) =>
+  option.addEventListener("click", handleOptionSelected)
+);
+
+document
+  .querySelector(".dropdown .title")
+  .addEventListener("change", handleTitleChange);
